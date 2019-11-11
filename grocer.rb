89b84a004a151d992e69e93ff6d274e3429e8d4a
Cane Rosso <astor@usa.com>
2019-11-11 06:01:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,12 +57,16 @@ end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
-  
-  cart.keys.each do |item|
-    if cart[item][:clearance]
-      cart[item][:price] = (cart[item][:price]*0.80).round(2)
+    i = 0
+  while i < cart.length do
+    item = cart[i]
+    if item[:clearance]
+      discounted_price = ((1 - CLEARANCE_ITEM_DISCOUNT_RATE) * item[:price]).round(2)
+        item[:price] = discounted_price
     end
+    i += 1
   end
+
   cart
    # REMEMBER: This method **should** update cart
 end
@@ -74,15 +78,7 @@ def checkout(cart, coupons)
   # * consolidate_cart
   # * apply_coupons
   # * apply_clearance
-  consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
-  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
-
-  total = 0.0
-  cart_with_discounts_applied.keys.each do |item|
-    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
-  end
-  total > 100.00 ? (total * 0.90).round : total
+ 
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
 end
