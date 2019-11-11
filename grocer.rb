a97b8ccf77e03056dcfa4de5778d4e4a4adaf801
Cane Rosso <astor@usa.com>
2019-11-11 05:50:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,7 +31,24 @@ end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
-  
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
+        else
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
   
   
   # REMEMBER: This method **should** update cart
